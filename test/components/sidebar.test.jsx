3005480ef7e861a48f7a45ddc1c0d58965a93d4a
Barret Rennie <barret@brennie.ca>
2017-05-19 23:46:56 -0600

    Add a test for handling browser.tabs.onMoved
    Extract out common test data (e.g., tabs) into globals and re-assign
    every test run.

--- test/components/sidebar.test.jsx
+++ test/components/sidebar.test.jsx
@@ -5,11 +5,14 @@ import React from "react";
 import browser from "sinon-chrome";
 
 import SideBar from "components/sidebar";
+import TabInfo from "components/tabInfo";
 
 afterEach(() => browser.reset());
 
+const testWindowId = 123;
+
 test("<SideBar /> with 0 tabs", done => {
-  browser.windows.get.withArgs(123, { populate: true }).returns(
+  browser.windows.get.withArgs(testWindowId, { populate: true }).returns(
     new Promise(resolve =>
       resolve({
         tabs: [],
@@ -17,7 +20,7 @@ test("<SideBar /> with 0 tabs", done => {
     ),
   );
 
-  const sidebar = mount(<SideBar windowId={123} />);
+  const sidebar = mount(<SideBar windowId={testWindowId} />);
 
   setTimeout(() => {
     try {
@@ -30,38 +33,79 @@ test("<SideBar /> with 0 tabs", done => {
   });
 });
 
-test("<SideBar /> with multiple tabs", done => {
-  browser.windows.get.withArgs(123, { populate: true }).returns(
-    new Promise(resolve =>
-      resolve({
-        tabs: [
-          {
-            id: 0,
-            active: true,
-            url: "http://example.com",
-            favIconUrl: undefined,
-            title: "example.com",
-          },
-          {
-            id: 1,
-            url: "http://example.com/foo",
-            favIconUrl: "http://example.com/favicon.ico",
-            title: "example.com/foo",
-          },
-        ],
-      }),
-    ),
-  );
+describe("<SideBar /> with multiple tabs", () => {
+  const testTabs = [
+    {
+      id: 0,
+      active: true,
+      url: "http://example.com",
+      title: "example.com",
+    },
+    {
+      id: 1,
+      url: "http://example.com/foo",
+      favIconUrl: "http://example.com/favicon.ico",
+      title: "example.com/foo",
+    },
+    {
+      id: 2,
+      url: "http://example.com/bar",
+      title: "example.com/bar",
+    },
+  ];
 
-  const sidebar = mount(<SideBar windowId={123} />);
+  beforeEach(() => {
+    browser.windows.get
+      .withArgs(123, { populate: true })
+      .returns(new Promise(resolve => resolve({ tabs: testTabs.slice() })));
+  });
 
-  setTimeout(() => {
-    try {
-      expect(browser.windows.get.called).toBe(true);
-      expect(sidebar.find("#tabs-list").children().length).toEqual(2);
-      done();
-    } catch (e) {
-      done.fail(e);
-    }
+  test("rendering", done => {
+    const sidebar = mount(<SideBar windowId={testWindowId} />);
+
+    setTimeout(() => {
+      try {
+        expect(browser.windows.get.called).toBe(true);
+        expect(sidebar.find("#tabs-list").children().length).toEqual(
+          testTabs.length,
+        );
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+  });
+
+  test("moving tabs", done => {
+    const sidebar = mount(<SideBar windowId={testWindowId} />);
+
+    setTimeout(() => {
+      try {
+        expect(browser.windows.get.called).toBe(true);
+
+        browser.tabs.onMoved.trigger(testTabs[0].id, {
+          windowId: testWindowId,
+          fromIndex: 0,
+          toIndex: 2
+        });
+
+        expect(sidebar.state().tabIds).toEqual([
+          testTabs[1].id,
+          testTabs[2].id,
+          testTabs[0].id,
+        ]);
+
+        const tabs = sidebar.find(TabInfo);
+
+        expect(tabs.length).toBe(3);
+        expect(tabs.at(0).props().tabId).toBe(testTabs[1].id);
+        expect(tabs.at(1).props().tabId).toBe(testTabs[2].id);
+        expect(tabs.at(2).props().tabId).toBe(testTabs[0].id);
+
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
   });
 });
